7cd8f8d5cb2be40218596337ff6b8ccb975a3da1
Chuvakatsup <danieel_lugo@hotmail.com>
2026-03-04 19:22:09 -0700

    Agregue un print

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 print("Hola mi nombre es Leonel y esta es mi colaboración :P")
 print ("Recuerden seguirse en sus perfiles de GitHub, hacer colaboración y obtener su certificado!!!")
+print("Me gustan los tacos")
